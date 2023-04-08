55b2d3db648f8cc0819728b4ddf1e068bdfc8f39
Prasham1710 <jainprasham17@gmail.com>
2023-04-08 18:51:13 +0530

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
-
+import Sidebar from '@/components/Sidebar'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Sidebar>
+      <Component {...pageProps} />
+    </Sidebar>
+  );
 }
