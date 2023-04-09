7ec32c554fe87978156925adbe2914b832972191
Prasham1710 <jainprasham17@gmail.com>
2023-04-10 00:41:03 +0530

    update

--- components/TopCards.jsx
+++ components/TopCards.jsx
@@ -6,7 +6,7 @@ const TopCards = () => {
       <div className='lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg'>
         <div className='flex flex-col w-full pb-4'>
           <p className='text-2xl font-bold'>$7,846</p>
-          <p className='text-gray-600'>Daily Revene</p>
+          <p className='text-gray-600'>Daily Revenue</p>
         </div>
         <p className='  bg-cyan-300 text-gray-200 flex justify-center items-center p-2 rounded-lg'>
           <span className='text-green-700 text-lg'> +14% </span>
@@ -15,7 +15,7 @@ const TopCards = () => {
   <div className='lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg'>
         <div className='flex flex-col w-full pb-4'>
           <p className='text-2xl font-bold'>$1,437,876</p>
-          <p className='text-gray-600'>yearly Revene</p>
+          <p className='text-gray-600'>yearly Revenue</p>
         </div>
         <p className=' bg-cyan-300 text-gray-200 flex justify-center items-center p-2 rounded-lg'>
           <span className='text-green-700 text-lg'> +12% </span>
